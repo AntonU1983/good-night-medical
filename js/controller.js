7dbc56dc524aad735bfc4bd594cc6f77a563c1c4
Artyom <cdndr@CDs-iMac.local>
2016-06-23 23:41:52 +0600

    v2.0

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,23 @@
 var mainApp = angular.module('mainApp', []);
 
-mainApp.controller('MainCtrl', function($scope, $http) {
+mainApp.controller('MainCtrl', function($scope) {
 
   $scope.returnHeight = function() {return {height: window.innerHeight + 40 + 'px'};}
+
+})
+
+mainApp.controller('SliderCtrl', function($scope){
+
+  $scope.count = 1;
+
+  $scope.prev = function() {
+    if ($scope.count > 1) {$scope.count--;}
+    else                  {$scope.count = 3;}
+  }
+
+  $scope.next = function() {
+    if ($scope.count < 3) {$scope.count++;}
+    else                  {$scope.count = 1;}
+  }
 
 })
