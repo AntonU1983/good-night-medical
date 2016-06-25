727f1beb69dd60adc047d5eb21d4c589c8a1ae82
Artyom <cdndr@CDs-iMac.local>
2016-06-25 20:52:02 +0600

    v3.3

--- front-end/js/controller.js
+++ front-end/js/controller.js
@@ -1,5 +1,28 @@
 var landingApp = angular.module('landingApp', []);
+var catalogApp = angular.module('catalogApp', []);
+
 
 landingApp.controller('HomeCtrl', function($scope) {
+  // Вычисляем высоту видимой части окна браузера
+  // Плюс 40px чтобы точно снизу обрезать дугой фон
   $scope.returnHeight = function() {return {height: window.innerHeight + 40 + 'px'};}
+})
+
+catalogApp.controller('CatalogCtrl', function($scope){
+  // Вычисляем высоту боковой панели
+  // Минус отступы и header
+  // Получаем красивую боковую панель
+  $scope.returnHeight = function() {return {height: window.innerHeight - 130 + 'px'};}
+
+  // Смена стилей для первой пары кнопок фильтрации
+  $scope.returnClassBtnFilterOne = function(number) {
+    if ($scope.filterOne == number) {return 'form-btn-active'}
+    else  {return 'form-btn-inactive'}
+  }
+
+  // Смена стилей для второй пары кнопок фильтрации
+  $scope.returnClassBtnFilterTwo = function(number) {
+    if ($scope.filterTwo == number) {return 'form-btn-active'}
+    else  {return 'form-btn-inactive'}
+  }
 })
--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,3 +1,4 @@
+// Плавная прокрутка при нажатие на якорь
 $(document).ready(function() {
   $("a[href*='#']").click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -12,3 +13,38 @@ $(document).ready(function() {
     }
   });
 });
+
+// Логика взаимодействия слайдера в боковой панели страницы Catalog
+// div с #slider создает слайдер с двумя ползунками
+// Два input (#minCost и #maxCost) хранят в себе данные ползунков для AngularJS
+// Два div (#minCostDiv и #maxCostDiv) хранят в себе данные ползунков для вывода пользователю
+jQuery(document).ready(function() {
+  jQuery("#slider").slider({
+    min: 0,
+    max: 75,
+    values: [0, 75],
+    range: true,
+    create: function(event, ui) {
+      // Вносим данные когда создался slider
+      jQuery("input#minCost").val(jQuery("#slider").slider("values", 0));
+      jQuery("div#minCostDiv").text("$"+jQuery("#slider").slider("values", 0));
+      jQuery("input#maxCost").val(jQuery("#slider").slider("values", 1));
+      jQuery("div#maxCostDiv").text("$"+jQuery("#slider").slider("values", 1));
+    },
+    slide: function(event, ui) {
+      // Изменяем данные когда пользователь двигает ползунок
+      jQuery("input#minCost").val(jQuery("#slider").slider("values", 0));
+      jQuery("div#minCostDiv").text("$"+jQuery("#slider").slider("values", 0));
+      jQuery("input#maxCost").val(jQuery("#slider").slider("values", 1));
+      jQuery("div#maxCostDiv").text("$"+jQuery("#slider").slider("values", 1));
+    },
+    stop: function(event, ui) {
+      // Изменяем данные когда пользователь перестал двигать ползнуок
+      jQuery("input#minCost").val(jQuery("#slider").slider("values", 0));
+      jQuery("div#minCostDiv").text("$"+jQuery("#slider").slider("values", 0));
+      jQuery("input#maxCost").val(jQuery("#slider").slider("values", 1));
+      jQuery("div#maxCostDiv").text("$"+jQuery("#slider").slider("values", 1));
+    }
+  });
+});
+
