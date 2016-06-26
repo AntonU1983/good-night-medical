452e85218fe1fdef24c1a3f8f237e02a58fd6f14
Alexander V <alexvab@gmail.com>
2016-06-26 14:55:09 +0300

    moved unit of work into separate file

--- back-end/GenericBackend/GenericBackend.UnitOfWork/GoodNightMedical/IUnitOfWork.cs
+++ back-end/GenericBackend/GenericBackend.UnitOfWork/GoodNightMedical/IUnitOfWork.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using GenericBackend.DataModels.GoodNightMedical;
 using GenericBackend.Repository;
-using GenericBackend.Repository.GoodNightMedical;
 
 namespace GenericBackend.UnitOfWork.GoodNightMedical
 {
@@ -15,24 +14,5 @@ namespace GenericBackend.UnitOfWork.GoodNightMedical
         IMongoRepository<RentOption> RentOptions { get; }
         IMongoRepository<MachineOrder> MachineOrders { get; }
 
-    }
-
-    public class UnitOfWork : IUnitOfWork
-    {
-        private IMongoRepository<Machine> _machines;
-        private IMongoRepository<RentOption> _rentOptions;
-        private IMongoRepository<MachineOrder> _machineOrders;
-
-        public IMongoRepository<Machine> Machines
-        {
-            get { return _machines ?? (_machines = new MachineRepository()); }
-        }
-
-        public IMongoRepository<RentOption> RentOptions
-        {
-            get { return _rentOptions ?? (_rentOptions = new RentOptionRepository()); }
-        }
-
-        public IMongoRepository<MachineOrder> MachineOrders { get { return _machineOrders ?? (_machineOrders = new MachineOrderRepository()); } }
     }
 }
---
+++ back-end/GenericBackend/GenericBackend.UnitOfWork/GoodNightMedical/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using GenericBackend.DataModels.GoodNightMedical;
+using GenericBackend.Repository;
+using GenericBackend.Repository.GoodNightMedical;
+
+namespace GenericBackend.UnitOfWork.GoodNightMedical
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private IMongoRepository<Machine> _machines;
+        private IMongoRepository<RentOption> _rentOptions;
+        private IMongoRepository<MachineOrder> _machineOrders;
+
+        public IMongoRepository<Machine> Machines
+        {
+            get { return _machines ?? (_machines = new MachineRepository()); }
+        }
+
+        public IMongoRepository<RentOption> RentOptions
+        {
+            get { return _rentOptions ?? (_rentOptions = new RentOptionRepository()); }
+        }
+
+        public IMongoRepository<MachineOrder> MachineOrders
+        {
+            get { return _machineOrders ?? (_machineOrders = new MachineOrderRepository()); }
+        }
+    }
+}
