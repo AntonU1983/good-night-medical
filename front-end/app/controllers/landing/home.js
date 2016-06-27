8084f53bd91617a8098f15a5e27583ede32c6a2c
Artyom <cdndr@CDs-iMac.local>
2016-06-27 22:30:42 +0600

    v3.9

---
+++ front-end/app/controllers/landing/home.js
@@ -0,0 +1,5 @@
+landingApp.controller('HomeCtrl', function($scope) {
+  // Вычисляем высоту видимой части окна браузера
+  // Плюс 40px чтобы точно снизу обрезать дугой фон
+  $scope.returnHeight = function() {return {height: window.innerHeight + 40 + 'px'};}
+})
