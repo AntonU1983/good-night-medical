6d75e05c452a3938b610d747ec8a7e70a87484f9
CDNDR <cdndr@CDs-iMac.local>
2016-07-01 02:00:53 +0600

    edit admin panel

---
+++ admin/app/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('app', ['angular-ellipses']);
+
+app.controller('Ctrl', function($scope, $http) {
+
+  var view = 0;
+  var pageUrl = [
+    'dashboard.html',
+    'data-contact-us-msg.html',
+    'data-rent-msg.html',
+    'data-help-msg.html',
+    'machine-add.html',
+    'machine-edit.html'];
+
+  $http.get('content/data/contact-msg.json').success(function(data){$scope.contactMsg = data;});
+  $http.get('content/data/rent-msg.json').success(function(data){$scope.rentMsg = data;});
+  $http.get('content/data/help-msg.json').success(function(data){$scope.helpMsg = data;});
+
+  $scope.pageSelection = function(pageNumber) {view = pageNumber;}
+  $scope.returnViewPage = function() {return 'views/' + pageUrl[view];}
+  $scope.returnActiveClass = function(pageNumber) {return (pageNumber == view) ? 'active' : '';}
+
+})
+
