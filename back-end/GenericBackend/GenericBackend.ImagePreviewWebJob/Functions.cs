e5b93db2ac13ba96c492e8c24984aa4a42b0038c
Alexander V <alexvab@gmail.com>
2016-07-03 15:29:48 +0300

    finished azure webjob for resizing

--- back-end/GenericBackend/GenericBackend.ImagePreviewWebJob/Functions.cs
+++ back-end/GenericBackend/GenericBackend.ImagePreviewWebJob/Functions.cs
@@ -4,17 +4,30 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImageResizer;
 using Microsoft.Azure.WebJobs;
 
 namespace GenericBackend.ImagePreviewWebJob
 {
     public class Functions
     {
-        // This function will get triggered/executed when a new message is written 
-        // on an Azure Queue called queue.
-        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        private const string InputBlobPath =  "goodnightmedical/{name}.{ext}";
+        private const string OutputBlobPath = "goodnightmedical-preview/{name}-60x60.{ext}";
+
+        public static void CreatePreviewImage([BlobTrigger(InputBlobPath)] Stream input,
+        [Blob(OutputBlobPath, FileAccess.Write)] Stream output)
         {
-            log.WriteLine(message);
+            ResizeImage(input, output, 60);
+        }
+        private static void ResizeImage(Stream input, Stream output, int width)
+        {
+            var instructions = new Instructions
+            { 
+                Width = width,
+                Mode = FitMode.Carve,
+                Scale = ScaleMode.Both,
+            };
+            ImageBuilder.Current.Build(new ImageJob(input, output, instructions));
         }
     }
 }
