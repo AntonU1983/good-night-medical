41fc7bc78c28d6a7c5a5fb718439fd5eb163abbd
CDNDR <skritkaz@gmail.com>
2016-07-27 01:29:42 +0600

    add structure lp3

---
+++ front-end-3/app/app.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  var app = angular.module('app', [
+    'ui.router'
+  ]);
+
+  // Handle routing errors and success events
+  app.run([function () {
+    // Include $route to kick start the router.
+  }]);
+
+})();
