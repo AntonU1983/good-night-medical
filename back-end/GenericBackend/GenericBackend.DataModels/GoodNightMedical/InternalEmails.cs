d77658490f555d9bb909e90cd4030dc608f1d27a
Andrey Rubayko <tograilx@gmail.com>
2016-07-30 23:57:27 +0300

    fix autodeploy

---
+++ back-end/GenericBackend/GenericBackend.DataModels/GoodNightMedical/InternalEmails.cs
@@ -0,0 +1,11 @@
+﻿using GenericBackend.Core;
+
+namespace GenericBackend.DataModels.GoodNightMedical
+{
+    public class InternalEmails :  MongoEntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
