e4ec8d865584acbac3ac3180c2cb87ea4cc50851
CDNDR <skritkaz@gmail.com>
2016-08-06 04:26:05 +0600

    website: add footer

--- website/app/controllers/modules/footer.js
+++ website/app/controllers/modules/footer.js
@@ -5,10 +5,61 @@
   angular.module('app').controller(controllerId, ['$scope', footerCtrl]);
 
   function footerCtrl($scope) {
-    $scope.content = content;
+    $scope.aboutUs = aboutUs;
+    $scope.copyright = copyright;
+    $scope.statesLinks = statesLinks;
+    $scope.socialLinks = socialLinks;
   }
 
-  var content = '';
+  var aboutUs = 'Good Night Medical is proud to be nationally known for its strength and knowledge in the area of sleep medicine and home ventilation. Good Night Medical provides the required services needed to diagnose a Sleep Apnea patient in their home. Once diagnosed, Good Night Medical provides CPAP treatment via home ventilation and oxygen equipment to our patients. Good Night Medical is dedicated to saving patients on their healthcare costs. This includes fighting for their insurance coverage, offering low cost options for those without insurance, and finding ways to cut the costs of these products through innovative recycling, refurbishing, or sourcing.';
+
+  var copyright = 'Copyright © 2016 Good Night Medical. All Rights Reserved.';
+
+  var someObjUrl = {
+    title: 'Some Title For Links',
+    href: false,
+    array: [{
+      text: '',
+      uiSref: ''
+    }, {
+      text: '',
+      uiSref: ''
+    }]
+  };
+
+  var statesLinks = [{
+    text: 'Home',
+    uiSref: 'home'
+  }, {
+    text: 'About Us',
+    uiSref: ''
+  }, {
+    text: 'Locations',
+    uiSref: ''
+  }, {
+    text: 'Rent',
+    uiSref: 'rent.catalog'
+  }, {
+    text: 'Auto Re-Supply',
+    uiSref: 'reSupply.info'
+  }, {
+    text: 'Home Sleep',
+    uiSref: 'sleepTest.info'
+  }, {
+    text: 'Testimonials',
+    uiSref: ''
+  }];
+
+  var socialLinks = [{
+    text: 'Facebook',
+    href: 'https://facebook.com/goodnightmed/'
+  }, {
+    text: 'Twitter',
+    href: 'https://twitter.com/goodnightmed'
+  }, {
+    text: 'Pinterest­',
+    href: 'https://pinterest.com/goodnightmed/'
+  }];
 
 })();
 
