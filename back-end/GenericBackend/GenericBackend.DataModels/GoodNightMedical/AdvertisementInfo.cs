c2f9341ce3d295d667a05a183816eecd70cfb4d6
Anton Udalov <audalov@medavante.com>
2016-08-08 17:15:12 +0300

    GK-13 - Created AdvertisementInfo Model

---
+++ back-end/GenericBackend/GenericBackend.DataModels/GoodNightMedical/AdvertisementInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBackend.DataModels.GoodNightMedical
+{
+    public class AdvertisementInfo
+    {
+        public string CampaignId { get; set; }
+        public string AdgroupId { get; set; }
+        public string Creative { get; set; }
+        public string Keyword { get; set; }
+        public string MatchType { get; set; }
+        public string Device { get; set; }
+        public string AdPosition { get; set; }
+        public string Location { get; set; }
+    }
+}
